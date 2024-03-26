519985d58fe0760b5ac461cb2e9d6f6ea423615e
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-26 13:29:44 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const App = () => {
+  const [data, setData] = useState([]);
+  let userData;
+  useEffect(() => {
+    (async function () {
+      const response = await axios({
+        url: "https://reqres.in/api/users",
+        method: "GET",
+      });
+      const dataObtained = response.data;
+      userData = dataObtained.data;
+      //   setData(dataObtained.data);
+      console.log(data);
+    })();
+  }, []);
+  const handleClick = () => {
+    setData(userData);
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <>
+      <div className="btn-wrapper">
+        <h1>Blue Whales</h1>
+        <button onClick={handleClick}>Get User List</button>
+      </div>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Avatar</th>
+        </tr>
+        {data.length > 0 &&
+          data.map((user) => {
+            return (
+              <>
+                <tr>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <img src={user.avatar} />
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+      </table>
+    </>
+  );
+};
 
-export default App
+export default App;
